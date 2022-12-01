ab51eaf2a7c4583964c48b08d3e875fbb2cdeac7
Christophe MICHEL <1050366+christopheml@users.noreply.github.com>
2022-12-01 14:26:54 +0100

    [Day 1] Simplified list transformation

--- src/main/kotlin/com/github/christopheml/aoc2022/day01/Calories.kt
+++ src/main/kotlin/com/github/christopheml/aoc2022/day01/Calories.kt
@@ -7,16 +7,12 @@ fun main() {
     val sortedCalories = PuzzleInput(1)
         .asList()
         .split("")
-        .map { it.map { element -> element.toInt() } }
-        .map { it.sum() }
+        .map { it.sumOf(String::toInt) }
         .sortedDescending()
 
     val mostCalories = sortedCalories.first()
-
     println("The elf with most calories has $mostCalories calories")
 
     val threeMostCalories = sortedCalories.subList(0, 3).sum()
-
     println("The three elves with most calories have $threeMostCalories calories")
-
 }
